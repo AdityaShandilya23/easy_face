80f19dea096e44c22691d3df294a97fefbcd0d46
Aditya Shandilya <85393000+AdityaShandilya23@users.noreply.github.com>
2023-10-08 19:34:47 +0530

    Add files via upload
    File containing the face_processing class

---
+++ easy_face.py
@@ -0,0 +1,58 @@
+import face_recognition
+import cv2
+import threading
+
+
+
+
+class Cam:
+	def __init__(self,index):
+		self.cap = cv2.VideoCapture(index)
+
+	def get_frame(self):
+		ret, frame = self.cap.read()
+		return frame
+
+class face_proccesing:
+	def __init__(self,cam):
+		self.known_face_encoding =[]
+		self.known_face_name = []
+		self.cam = cam
+	
+	def add_face(self,pic,name):
+		image = face_recognition.load_image_file(pic)
+		encoding = face_recognition.face_encodings(image)[0]
+		self.known_face_name.append(name)
+		self.known_face_encoding.append(encoding)
+		
+	
+	def find_face(self,frame):
+		#frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25) #Uncomment this line to reduce the size of frame
+		face_locations = face_recognition.face_locations(frame)
+		face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+	# Loop through each face found in the frame
+		for face_encoding in face_encodings:
+			# Compare the face with known faces
+			matches = face_recognition.compare_faces(self.known_face_encoding, face_encoding)
+			name = "Unknown"
+
+		# Find the best match
+			if True in matches:
+				first_match_index = matches.index(True)
+				name = self.known_face_name[first_match_index]
+			print(name) #Use this variable to get the name of face	
+	def fast_face(self):
+		thread = threading.Thread(target=self.find_face,args=(self.cam.get_frame(),))
+		thread.start()
+
+
+
+
+
+if __name__ == "__main__":
+	camera = Cam(0)
+	detect = face_proccesing(camera)
+	detect.add_face('path to file',"name")		
+	while True:
+		detect.fast_face()
